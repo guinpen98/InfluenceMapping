01d38ee5d365f2c77e022f6d925bc25e09da961c
guinpen98 <yuri.pocha.0908@gmail.com>
2022-02-03 19:32:29 +0900

    配列の変更

---
+++ InfluenceMapping/property.h
@@ -0,0 +1,8 @@
+#ifndef INFLUENCE_MAPPING_PROPERTY_H
+#define INFLUENCE_MAPPING_PROPERTY_H
+namespace influenceMapping {
+
+	constexpr int walk_fps = 20;
+
+}
+#endif // !INFLUENCE_MAPPING_PROPERTY_H
