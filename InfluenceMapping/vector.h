8d17d3b03e3d109c6b3334f473889005daedfc27
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2022-02-13 22:50:42 +0900

    方向ベクトルの使用

--- InfluenceMapping/vector.h
+++ InfluenceMapping/vector.h
@@ -7,30 +7,33 @@ namespace influenceMapping {
 	{
 		double x = 0, y = 0;
 
-		Vec2() = default;
+		constexpr Vec2() = default;
 
-		Vec2(const double ix, const double iy)
+		constexpr Vec2(const double ix, const double iy)
 			:x(ix), y(iy) {}
 
-		double distance(const Vec2& coord)const {
+		constexpr double distance(const Vec2& coord)const {
 			return ((x - coord.x) * (x - coord.x) + (y - coord.y) * (y - coord.y));
 		}
-		double centerDistance()const {
+		constexpr double centerDistance()const {
 			return ((x - (window_square_w / 2 - 0.5)) * (x - (window_square_w / 2 - 0.5)) + (y - (window_square_w / 2)) * (window_square_w / 2));
 		}
 
-		bool isEqual(const Vec2& coord) const {
+		constexpr bool isEqual(const Vec2& coord) const {
 			return (x == coord.x && y == coord.y);
 		}
 
-		Vec2 operator+(const Vec2& coord)const {
+		constexpr Vec2 operator+(const Vec2& coord)const {
 			return Vec2(x + coord.x, y + coord.y);
 		}
+		constexpr Vec2 operator/(const double n)const {
+			return Vec2(x / n, y / n);
+		}
 	};
-	const Vec2 right_coord = Vec2(1.0, 0.0);
-	const Vec2 left_coord = Vec2(-1.0, 0.0);
-	const Vec2 down_coord = Vec2(0.0, 1.0);
-	const Vec2 up_coord = Vec2(0.0, -1.0);
+	constexpr Vec2 right_coord = Vec2{ 1.0, 0.0 };
+	constexpr Vec2 left_coord = Vec2{ -1.0, 0.0 };
+	constexpr Vec2 down_coord = Vec2{ 0.0, 1.0 };
+	constexpr Vec2 up_coord = Vec2{ 0.0, -1.0 };
 }
 
 #endif // !INFLUENCE_MAPPING_VECTOR_H
